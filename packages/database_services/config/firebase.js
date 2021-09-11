4beb8e492be1f1404e5859014232deddaa8e5745
nipunayf <nipuna.18@cse.mrt.ac.lk>
2021-09-12 03:28:34 +0530

    ADD - yarn and package.json

--- packages/database_services/config/firebase.js
+++ packages/database_services/config/firebase.js
@@ -1,8 +1,18 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("./firebase_secret.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
-});
+})
+
+module.exports = {
+    userStore: admin.app().firestore().collection('users'),
+    itineraryStore: admin.app().firestore().collection('itineraries'),
+    auth: admin.app().auth()
+};
+
+
+
+
 
---
+++ packages/database_services/utils/message-template.js
@@ -0,0 +1,29 @@
+/**
+ * Output message template for a success message
+ *
+ * @param {Response} response - http response object
+ * @param {object} results - json object of the results
+ * @param {string} message - message to display
+ * @param {Number} status - status code of the response. default = 200
+ */
+const successMessage = (response, results, message = "", status = 200) => {
+    output = { results : results, message: message };
+    return response.status(status).send(output);
+}
+
+/**
+ * Output message tempalte for a error message
+ *
+ * @param {Response} response - http response object
+ * @param {string} message - error message
+ * @param {Number} status - status code of the response. default = 400
+ */
+const errorMessage = (response, message, status = 400) => {
+    output = { object : null, message: message }
+    return response.status(status).send(output);
+}
+
+module.exports = {
+    successMessage,
+    errorMessage
+}
