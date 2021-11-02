aac2ee0852e1ade5736bba04cdaed5e51cb1dcd1
nipunayf <nipuna.18@cse.mrt.ac.lk>
2021-11-02 22:41:32 +0530

    ADD - travel history

--- packages/app_web/src/screens/MyItineraries.jsx
+++ packages/app_web/src/screens/MyItineraries.jsx
@@ -6,7 +6,8 @@ import {
     InputGroup,
     InputLeftElement,
     Link,
-    Select, SimpleGrid,
+    Select,
+    SimpleGrid,
     Spacer,
     VStack
 } from "@chakra-ui/react";
@@ -15,13 +16,53 @@ import DatePicker from "react-datepicker";
 
 import 'react-datepicker/dist/react-datepicker.css';
 import '../components/Destination/datepicker.css';
-import {useState} from "react";
+import React, {useState} from "react";
 import Navbar from "../containers/Navbar/Navbar";
 import NoResultsSplash from "../components/Alert/NoResultsSplash";
+import {StateEnum} from "../utils/constants";
+import {useFormik} from "formik";
+import PlaceCard from "../containers/Search/PlaceCard";
+import ItineraryCard from "../components/Itinerary/ItineraryCard";
+import {connect} from "react-redux";
 
-function SearchBar() {
+function SearchBar({cached, setResult}) {
     const [dateRange, setDateRange] = useState([null, null]);
     const [startDate, endDate] = dateRange;
+
+    const formik = useFormik({
+        initialValues: {
+            state: '',
+            keyword: '',
+            startDate: startDate,
+            endDate: endDate
+        },
+
+        onSubmit: async values => {
+            formik.setSubmitting(true);
+            const arr = [];
+
+            // Need to to query the database to search the reviewed itineraries
+            // noinspection EqualityComparisonWithCoercionJS
+            for (let id in cached) {
+                const info = cached[id];
+                // if (info.state )
+                arr.push({...cached[id], id})
+                console.log(values);
+            }
+            // if (values.state == StateEnum.REVIEWED || values.state == StateEnum.ANY) {
+            //     console.log(values);
+            // } else {
+            //
+            // }
+            setResult(arr);
+            formik.setSubmitting(false);
+        }
+    });
+
+    const onChange = (dates) => {
+        formik.setFieldValue('startDate', dates[0])
+        formik.setFieldValue('endDate', dates[1])
+    };
 
     return (
         <HStack w={'80%'} h={65} borderWidth="1px" borderRadius="lg" boxShadow={'lg'} p={4}>
@@ -32,12 +73,11 @@ function SearchBar() {
                     pt={2}
                 />
                 <Input
+                    id={"keyword"}
                     type="search"
                     placeholder="Search Itineraries"
                     isRequired
-                    // onChange={((e) => {setKeyword(e.target.value)}).bind(this)}
-                    // value={keyword}
-                    // onKeyPress={((e) => {if (e.key === 'Enter' && keyword.length !== 0) {history.push(`/search/${keyword}`, { keyword: keyword }); setKeyword('')}}).bind(this)}
+                    onChange={formik.handleChange}
                 />
             </InputGroup>
             <Spacer/>
@@ -46,28 +86,30 @@ function SearchBar() {
                 <DatePicker
                     w={300}
                     selectsRange={true}
-                    startDate={startDate}
-                    endDate={endDate}
-                    onChange={(update) => {
-                        setDateRange(update);
-                    }}
+                    selected={formik.values.startDate}
+                    onChange={onChange}
+                    startDate={formik.values.startDate}
+                    endDate={formik.values.endDate}
                     isClearable={true}
                 />
             </HStack>
             <Spacer/>
             <HStack><Heading size={14}>State:</Heading>
-                <Select placeholder="State" w={36}>
-                    <option value="1">Inactive</option>
-                    <option value="2">Incompatible</option>
-                    <option value="3">Active</option>
-                    <option value="4">To Be Reviewed</option>
-                    <option value="5">Reviewed</option>
+                <Select id="state" placeholder="Select state" w={36} onChange={formik.handleChange} isInvalid={formik.values['state'] === ""}>
+                    <option value={0}>Any</option>
+                    <option value={StateEnum.INACTIVE}>Inactive</option>
+                    <option value={StateEnum.INCOMPATIBLE}>Incompatible</option>
+                    <option value={StateEnum.ACTIVE}>Active</option>
+                    <option value={StateEnum.TO_BE_REVIEWED}>To Be Reviewed</option>
+                    <option value={StateEnum.REVIEWED}>Reviewed</option>
                 </Select>
             </HStack>
-            <Spacer />
+            <Spacer/>
             <IconButton
                 icon={<Search2Icon/>}
-                // onClick={() => { if (keyword.length !== 0) {history.push(`/search/${keyword}`, { keyword: keyword }); setKeyword('')} }}
+                isDisabled={formik.values['state'] === ""}
+                onClick={formik.handleSubmit}
+                isLoading={formik.isSubmitting}
                 bg="secondary.light"
                 color="white"/>
             <Link/>
@@ -75,20 +117,25 @@ function SearchBar() {
     );
 }
 
-function ItineraryContainer() {
-    return(
-        <SimpleGrid columns={{sm: 1, md: 2, lg: 3}} spacing="40px" pb={5} pt={8}>
-        </SimpleGrid>
-    );
-}
+function MyItineraries({itineraries}) {
+    const [resultItineraries, setResultItineraries] = useState([]);
 
-export default function MyItineraries() {
     return (<>
-        <Navbar />
+        <Navbar/>
         <VStack pt={20} align={'center'} w={'100%'}>
-            <SearchBar/>
-            {/*<ItineraryContainer />*/}
-            <NoResultsSplash message={'Enter valid keywords'}/>
+            <SearchBar cached={itineraries} setResult={setResultItineraries}/>
+            {resultItineraries.length === 0 ? <NoResultsSplash message={'Enter valid keywords'}/>
+                : <SimpleGrid columns={{base: 1, md: 2, lg: 3}} spacing={10} pt={5} px={5} pb={4}>
+            {resultItineraries.map(itinerary => <ItineraryCard id={itinerary.id} info={itinerary}/>)}
+                </SimpleGrid>}
         </VStack>
     </>);
 }
+
+const mapStateToProps = state => {
+    return {
+        itineraries: state.profile.itineraries,
+    };
+};
+
+export default connect(mapStateToProps, null)(MyItineraries);
