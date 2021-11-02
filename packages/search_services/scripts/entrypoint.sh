1a78e1aa51ca6c5c310c531cd6b6c392fe2ac724
Dasith Edirisinghe <dasith@liveroom.xyz>
2021-11-02 07:45:55 +0530

    deployment config updated

--- packages/search_services/scripts/entrypoint.sh
+++ packages/search_services/scripts/entrypoint.sh
@@ -4,4 +4,4 @@ set -e # exit if errors happen anywhere
 python3 manage.py collectstatic --noinput
 python manage.py migrate
 
-uwsgi --socket :9000 --workers 4 --master --enable-threads --module search_services.wsgi
+uwsgi --socket :8000 --workers 4 --master --enable-threads --module search_services.wsgi
